141657d924ec120b54d1e0ddf49a13edb99d6273
Felipe Cabuto <felipecabuto@gmail.com>
2024-04-30 19:27:01 -0300

    inicialização do projeto spring boot

---
+++ src/main/java/com/bilheteunico/SPTrans/SpTransApplication.java
@@ -0,0 +1,13 @@
+package com.bilheteunico.SPTrans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpTransApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpTransApplication.class, args);
+	}
+
+}
